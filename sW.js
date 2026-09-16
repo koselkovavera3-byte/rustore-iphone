0e1025d3c855f810654da3cf3aa10bad34512263
koselkovavera3-byte <koselkovavera3@gmail.com>
2026-09-16 12:43:30 +0300

    Create sW.js

---
+++ sW.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "rustore-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
